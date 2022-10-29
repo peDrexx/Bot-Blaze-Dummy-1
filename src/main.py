d562327c4b1aec9b2b9f476005b04125d186873d
peDrexx <60234950+peDrexx@users.noreply.github.com>
2022-10-29 17:55:04 -0300

    Bot funfando tranquilo

--- src/main.py
+++ src/main.py
@@ -2,8 +2,22 @@ import requests
 import json
 import time as tm
 
-url = 'https://blaze.com/api/roulette_games/Double'
-response = requests.get(url)
-r = response.json()
+while True:
+    url = 'https://blaze.com/api/roulette_games/recent'
+    response = requests.get(url)
+    r = response.json()
+    arr = []
 
-print(r)
+    for x in range(len(r)):
+        val = r[x]['color']
+
+        if val == 0:
+            val = 'Branco'
+        if val == 1:
+            val = 'Vermelho'
+        if val == 2:
+            val = 'Preto'
+        arr.append(val)
+
+    print(arr)
+    tm.sleep(7)
